d323227031735e7ed13100748e4b18894c86dac7
abdulaliarafat <abdulaliarafat@gmail.com>
2025-05-12 15:51:01 +0600

    work with server

---
+++ my-phone-clint/src/Components/Phone.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+
+const Phone = () => {
+   const phone=useLoaderData()
+   console.log(phone)
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Phone;
