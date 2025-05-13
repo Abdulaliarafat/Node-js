3e37493945e09ab27ccfecef686e73ce11b8c465
abdulaliarafat <abdulaliarafat@gmail.com>
2025-05-13 22:06:49 +0600

    module complete with post and get logic

--- users-manegment-server/index.js
+++ users-manegment-server/index.js
@@ -3,19 +3,27 @@ const app=express();
 const cors=require('cors');
 const port=process.env.PORT || 3000;
 app.use(cors())
+app.use(express.json())
 app.get('/',(req,res)=>{
     res.send('users server is running')
 })
-app.get('/users',(req,res)=>{
-    res.send(users)
-})
-
 const users = [
   { id: 1, name: "Alice", email: "alice@example.com" },
   { id: 2, name: "Bob", email: "bob@example.com" },
   { id: 3, name: "Charls", email: "charlie@example.com" }
 ];
+app.get('/users',(req,res)=>{
+    res.send(users)
+})
+app.post('/users',(req,res)=>{
+ console.log('user post method');
+ console.log(req.body);
+ const newuser = req.body;
+ newuser.id=users.length+1;
+ users.push(newuser)
+ res.send(newuser)
 
+})
 
 app.listen(port,()=>{
     console.log(`users server in running port:${port}`);
