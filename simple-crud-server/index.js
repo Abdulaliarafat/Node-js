01d1b3ac48939c10a9b72bee27d97666bab727e2
abdulaliarafat <abdulaliarafat@gmail.com>
2025-05-15 06:47:07 +0600

    work with CURD oparation SERVER post ,get,delete

--- simple-crud-server/index.js
+++ simple-crud-server/index.js
@@ -10,6 +10,7 @@ app.use(express.json());
 // pass:YAidU2aFWzlXRSoi
 const uri = "mongodb+srv://simpleDBuser:YAidU2aFWzlXRSoi@cluster0.g02ycwa.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+
 const client = new MongoClient(uri, {
     serverApi: {
         version: ServerApiVersion.v1,
@@ -21,8 +22,24 @@ async function run() {
     try {
         await client.connect();
 
-        // ........optional just for confirmation is server running.........
+        const userCollection=client.db('userdb').collection('user');
 
+        app.get('/users', async(req,res)=>{
+            const cursor=userCollection.find();
+            const result=await cursor.toArray()
+            res.send(result)
+        })
+        app.post('/users',async(req,res)=>{
+            const newUser=req.body;
+            const result=await userCollection.insertOne(newUser)
+            res.send(result)
+        })
+        app.delete('/users/:id',(req,res)=>{
+            console.log(req.params)
+            const id=req.params.id
+            console.log(id)
+        })
+        // ........optional just for confirmation is server running.........
         // await client.db('admin').command({ ping: 1 });
         // console.log("Pinged your deployment. You successfully connected to MongoDB!");
 
