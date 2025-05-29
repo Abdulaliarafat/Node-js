4bb6353b901b9ee71b2ef18c273dd4caeb76f919
abdulaliarafat <abdulaliarafat@gmail.com>
2025-05-29 20:01:24 +0600

    impliment job details

--- job-portal-clinet/src/pages/ApplayJobs/JobApplay.jsx
+++ job-portal-clinet/src/pages/ApplayJobs/JobApplay.jsx
@@ -1,23 +1,48 @@
 import React from 'react';
 import { Link, useParams } from 'react-router';
 import useAuth from '../../Hooks/UseAuth';
+import axios from 'axios';
+import Swal from 'sweetalert2';
 
 
 const JobApplay = () => {
     const { user } = useAuth()
-    const job = useParams();
+    const jobId = useParams();
     console.log(user)
-    const handleApplyForm=(e)=>{
+    const handleApplyForm = (e) => {
         e.preventDefault()
-        const form=e.target;
-        const linkdin=form.linkdin.value;
-        const github=form.github.value;
-        const resume=form.resume.value;
-         console.log(linkdin,github,resume)
+        const form = e.target;
+        const linkdin = form.linkdin.value;
+        const github = form.github.value;
+        const resume = form.resume.value;
+        console.log(linkdin, github, resume)
+        const application = {
+            jobId,
+            application: user.email,
+            linkdin,
+            github,
+            resume,
+        }
+        axios.post('http://localhost:3000/applications', application)
+            .then(res => {
+                console.log(res.data)
+                if (res.data.insertedId) {
+                    Swal.fire({
+                        position: "center",
+                        icon: "success",
+                        title: "Your application has been submitted",
+                        showConfirmButton: false,
+                        timer: 1500
+                    });
+                }
+            })
+            .catch(error => {
+                console.log(error)
+            })
     }
     return (
         <div className='max-w-4xl mx-auto'>
-            <h1 className="text-4xl mb-5">Apply for this job : <Link className='text-blue-500' to={`/jobDetails/${job}`}>JOB</Link></h1>
+            <h1 className="text-4xl mb-5">Apply for this job : <Link className='text-blue-500' to={`/jobDetails/${jobId}`}>JOB</Link></h1>
             <form onSubmit={handleApplyForm}>
                 <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-full border p-4 space-y-3 my-5">
 
@@ -29,7 +54,7 @@ const JobApplay = () => {
 
                     <label className="label ml-40">Resume link</label>
                     <input type="url" name='resume' className="input w-xl mx-auto p-5" placeholder="Resume link" />
-                    <input type="submit"  className='btn btn-primary w-sm mx-auto' value="Apply" />
+                    <input type="submit" className='btn btn-primary w-sm mx-auto' value="Apply" />
                 </fieldset>
             </form>
         </div>
--- job-portal-clinet/src/pages/jobsDetails/JobDetails.jsx
+++ job-portal-clinet/src/pages/jobsDetails/JobDetails.jsx
@@ -2,11 +2,18 @@ import React from 'react';
 import { Link, useLoaderData } from 'react-router';
 
 const JobDetails = () => {
-    const {title,_id,company}=useLoaderData()
+    const {title,_id,company_logo,company}=useLoaderData()
     return (
-        <div>
-           <h1 className="text-xl font-bold">{company}</h1>
-           <Link to={`/jobApplay/${_id}`} className="btn btn-primary">Applay now</Link>
+        <div className='max-w-4xl mx-auto flex justify-center'>
+            <div className=''>
+                <img className='w-40' src={company_logo} alt="" />
+            </div>
+           <div className='space-y-1 py-3'>
+            <h1 className="text-lg font-bold">{company}</h1>
+           <p className='font-light text-md'>{title}</p>
+           <Link to={`/jobApplay/${_id}`} className="btn btn-primary mt-6">Applay now</Link>
+           </div>
+           
         </div>
     );
 };
