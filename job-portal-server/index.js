9d8e2b5af674813adc75ce21e4d41f5964b5dbbe
abdulaliarafat <abdulaliarafat@gmail.com>
2025-05-31 23:55:31 +0600

    work API done

--- job-portal-server/index.js
+++ job-portal-server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 app.use(cors());
 app.use(express.json());
@@ -24,13 +24,88 @@ async function run() {
     await client.connect();
 
     const jodsCollection=client.db("jobPortal").collection('jobs')
+
+    const applicationCollection=client.db("jobPortal").collection('applications')
+
     app.get('/jobs',async(req,res)=>{
-        const result= await jodsCollection.find().toArray()
+        const email=req.query.email;
+        const query={}
+        if(email){
+          query.hr_email=email;
+        }
+        const result= await jodsCollection.find(query).toArray()
         res.send(result)
+    })
+    app.get('/jobs/applications',async(req,res)=>{
+      const email=req.query.email;
+      const query={hr_email:email}
+      const jobs=await jodsCollection.find(query).toArray()
+      // not right way
+      for(const job of jobs){
+        const applicationQuery={jobId:job._id.toString()};
+        const applicationCount=await applicationCollection.countDocuments(applicationQuery)
+        job.application_count=applicationCount
+      }
+      res.send(jobs)
+    })
+    app.get('/jobs/:id',async(req,res)=>{
+      const id=req.params.id;
+      const query={_id:new ObjectId(id)};
+      const result=await jodsCollection.findOne(query);
+      res.send(result);
+    })
+    app.post('/jobs',async(req,res)=>{
+      const newJobs=req.body;
+      console.log(newJobs)
+      const result=await jodsCollection.insertOne(newJobs);
+      res.send(result);
+    })
+    // job application API
+    app.get('/applications',async(req,res)=>{
+      const email=req.query.email;
+      const query={
+        applicant: email
+      }
+      const result=await applicationCollection.find(query).toArray();
+      // bad way....
+      for(const application of result){
+        const jobId=application.jobId;
+        const jobquery={_id:new ObjectId(jobId)};
+        const job=await jodsCollection.findOne(jobquery);
+        application.company=job.company
+        application.title=job.title
+        application.company_logo=job.company_logo
+      }
+      res.send(result)
+    })
+    // get for count application.
+    app.get('/applications/job/:job_id',async(req,res)=>{
+      const job_id=req.params.job_id;
+      console.log(job_id);
+      const query={jobId:job_id};
+      const result=await applicationCollection.find(query).toArray();
+      res.send(result);
+    })
+    app.post('/applications',async(req,res)=>{
+    const application=req.body;
+    console.log(application)
+    const result=await applicationCollection.insertOne(application);
+    res.send(result)
+    })
+    app.patch('/applications/:id',async(req,res)=>{
+      const id=req.params.id;
+      const filter={_id:new ObjectId(id)};
+      const updateDoc={
+        $set:{
+          status:req.body.status
+        }
+      }
+      const result=await applicationCollection.updateOne(filter,updateDoc);
+      res.send(result)
     })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log("Pinged your deployment.You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
